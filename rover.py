8c2ee9ca1c15afc5377552b9bea344a41ae53d3e
oktN <oktn1c3@gmail.com>
2018-10-07 20:26:56 +0200

    Added functionality to handle planet surface (range) to use proper coordenates

--- rover.py
+++ rover.py
@@ -1,11 +1,22 @@
 class Rover:
     STEP = 1
-    DIRECTION_REFS = {'North': [0,STEP], 'East': [STEP,0], 'South': [0,-STEP], 'West': [-STEP,0]}
-    ORIENTATION_REFS = {'North': ['West','East'], 'East': ['North','South'], 'South': ['East','West'], 'West': ['South','North']}
+    SURFACE_AXIS = [0, 143]
+    POSITION_OPERATORS = {
+    'North': [0,STEP],
+    'East': [STEP,0],
+    'South': [0,-STEP],
+    'West': [-STEP,0]
+    }
+    TURN_REFERENCES = {
+    'North': ['West','East'],
+    'East': ['North','South'],
+    'South': ['East','West'],
+    'West': ['South','North']
+    }
 
     def __init__(self, given_position, given_direction):
-        self.position = given_position
-        self.direction = given_direction
+            self.position = given_position
+            self.direction = given_direction
 
     def broadcast_position(self):
         return self.position
@@ -15,20 +26,41 @@ class Rover:
 
     def command(self, commands):
         for command in commands:
+            if command == 'f':
+                self.position[0] += self.POSITION_OPERATORS.get(self.direction)[0]
+                if self.position[0] >= self.SURFACE_AXIS[1]:
+                    self.position[0] = self.SURFACE_AXIS[0]
 
-            if command == "f":
-                self.position[0] += self.DIRECTION_REFS.get(self.direction)[0]
-                self.position[1] += self.DIRECTION_REFS.get(self.direction)[1]
+                elif self.position[0] < self.SURFACE_AXIS[0]:
+                    self.position[0] = self.SURFACE_AXIS[1]
 
-            elif command == "b":
-                self.position[0] -= self.DIRECTION_REFS.get(self.direction)[0]
-                self.position[1] -= self.DIRECTION_REFS.get(self.direction)[1]
+                self.position[1] += self.POSITION_OPERATORS.get(self.direction)[1]
+                if self.position[1] >= self.SURFACE_AXIS[1]:
+                    self.position[1] = self.SURFACE_AXIS[0]
 
-            elif command == "r":
-                self.direction = self.ORIENTATION_REFS.get(self.direction)[1]
+                elif self.position[1] < self.SURFACE_AXIS[0]:
+                    self.position[1] = self.SURFACE_AXIS[1]
 
-            elif command == "l":
-                self.direction = self.ORIENTATION_REFS.get(self.direction)[0]
+            elif command == 'b':
+                self.position[0] -= self.POSITION_OPERATORS.get(self.direction)[0]
+                if self.position[0] >= self.SURFACE_AXIS[1]:
+                    self.position[0] = self.SURFACE_AXIS[0]
+
+                elif self.position[0] < self.SURFACE_AXIS[0]:
+                    self.position[0] = self.SURFACE_AXIS[1]
+
+                self.position[1] -= self.POSITION_OPERATORS.get(self.direction)[1]
+                if self.position[1] >= self.SURFACE_AXIS[1]:
+                    self.position[1] = self.SURFACE_AXIS[0]
+
+                elif self.position[1] < self.SURFACE_AXIS[0]:
+                    self.position[1] = self.SURFACE_AXIS[1]
+
+            elif command == 'r':
+                self.direction = self.TURN_REFERENCES.get(self.direction)[1]
+
+            elif command == 'l':
+                self.direction = self.TURN_REFERENCES.get(self.direction)[0]
 
             else:
-                print("Wrong command, doing nothing.")
+                print('Wrong command, doing nothing.')
--- spec/rover_spec.py
+++ spec/rover_spec.py
@@ -41,7 +41,7 @@ with description('Rover') as self:
         rover.command(['f'])
         current_position = rover.broadcast_position()
 
-        expected_position = [0, -1]
+        expected_position = [0, 143]
         expect(current_position).to(equal(expected_position))
 
     with it('moves forward facing East'):
@@ -63,7 +63,7 @@ with description('Rover') as self:
         rover.command(['f'])
         current_position = rover.broadcast_position()
 
-        expected_position = [-1, 0]
+        expected_position = [143, 0]
         expect(current_position).to(equal(expected_position))
 
     with it('moves backwards facing North'):
@@ -74,7 +74,7 @@ with description('Rover') as self:
         rover.command(['b'])
         current_position = rover.broadcast_position()
 
-        expected_position = [0, -1]
+        expected_position = [0, 143]
         expect(current_position).to(equal(expected_position))
 
     with it('moves backwards facing South'):
@@ -96,7 +96,7 @@ with description('Rover') as self:
         rover.command(['b'])
         current_position = rover.broadcast_position()
 
-        expected_position = [-1, 0]
+        expected_position = [143, 0]
         expect(current_position).to(equal(expected_position))
 
     with it('moves backwards Facing West'):
@@ -197,3 +197,91 @@ with description('Rover') as self:
         current_direction = rover.broadcast_direction()
         expected_direction = 'North'
         expect(current_direction).to(equal(expected_direction))
+
+    with it('reset coords to 0 when surpases 143 towards North'):
+        given_position = [0, 143]
+        given_direction = 'North'
+        rover = Rover(given_position, given_direction)
+
+        rover.command(['f'])
+        current_position = rover.broadcast_position()
+
+        expected_position = [0, 0]
+        expect(current_position).to(equal(expected_position))
+
+    with it('reset coords to 0 when surpases 143 towards East'):
+        given_position = [143, 0]
+        given_direction = 'East'
+        rover = Rover(given_position, given_direction)
+
+        rover.command(['f'])
+        current_position = rover.broadcast_position()
+
+        expected_position = [0, 0]
+        expect(current_position).to(equal(expected_position))
+
+    with it('reset coords to 143 when surpases 0 towards South'):
+        given_position = [0, 0]
+        given_direction = 'South'
+        rover = Rover(given_position, given_direction)
+
+        rover.command(['f'])
+        current_position = rover.broadcast_position()
+
+        expected_position = [0, 143]
+        expect(current_position).to(equal(expected_position))
+
+    with it('reset coords to 143 when surpases 0 towards West'):
+        given_position = [0, 0]
+        given_direction = 'West'
+        rover = Rover(given_position, given_direction)
+
+        rover.command(['f'])
+        current_position = rover.broadcast_position()
+
+        expected_position = [143, 0]
+        expect(current_position).to(equal(expected_position))
+
+    with it('reset coords to 143 when surpases 0 backwards North'):
+        given_position = [0, 0]
+        given_direction = 'North'
+        rover = Rover(given_position, given_direction)
+
+        rover.command(['b'])
+        current_position = rover.broadcast_position()
+
+        expected_position = [0, 143]
+        expect(current_position).to(equal(expected_position))
+
+    with it('reset coords to 143 when surpases 0 backwards East'):
+        given_position = [0, 0]
+        given_direction = 'East'
+        rover = Rover(given_position, given_direction)
+
+        rover.command(['b'])
+        current_position = rover.broadcast_position()
+
+        expected_position = [143, 0]
+        expect(current_position).to(equal(expected_position))
+
+    with it('reset coords to 0 when surpases 143 backwards South'):
+        given_position = [0, 143]
+        given_direction = 'South'
+        rover = Rover(given_position, given_direction)
+
+        rover.command(['b'])
+        current_position = rover.broadcast_position()
+
+        expected_position = [0, 0]
+        expect(current_position).to(equal(expected_position))
+
+    with it('reset coords to 0 when surpases 143 backwards West'):
+        given_position = [143, 0]
+        given_direction = 'West'
+        rover = Rover(given_position, given_direction)
+
+        rover.command(['b'])
+        current_position = rover.broadcast_position()
+
+        expected_position = [0, 0]
+        expect(current_position).to(equal(expected_position))
